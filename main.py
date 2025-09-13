01b7f3ad63ba053653a5ed9efe30cc93d9297860
Lucas Emilio Mendonça <mendoncae.metrocasa@gmail.com>
2025-09-13 15:03:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,20 @@ from langchain.docstore.document import Document
 
 load_dotenv()
 app = FastAPI()
-origins = ["http://localhost:8080", "http://127.0.0.1:8080"]
-app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
+origins = [
+    "https://superlative-rabanadas-c0ea6f.netlify.app",  
+    "http://localhost",
+    "http://localhost:8080", 
+    "http://localhost:5173", 
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  
+    allow_credentials=True, 
+    allow_methods=["*"],    
+    allow_headers=["*"],    
+)
 USERS = {
     "empresa": {"password": "123", "role": "user"},
     "admin": {"password": "admin123", "role": "admin"}
